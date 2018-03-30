59d9a6f4de230b0135e8b127fbe36bf1a7561cbd
Liman Adamu Labaran <adamu.labaran@callenssolutions.com>
2018-03-30 12:50:10 +0000

    Furnish Eloquent Article Repository

--- app/Articles/EloquentArticlesRepository.php
+++ app/Articles/EloquentArticlesRepository.php
@@ -3,14 +3,71 @@
 namespace App\Articles;
 
 use App\Article;
+use Elasticsearch\Client;
 use Illuminate\Database\Eloquent\Collection;
 
 class EloquentArticlesRepository implements ArticlesRepository
 {
+   private $search;
+
+   public function __construct(Client $client) {
+       $this->search = $client;
+   }
+
     public function search(string $query = ""): Collection
     {
-        return Article::where('body', 'like', "%{$query}%")
-            ->orWhere('title', 'like', "%{$query}%")
-            ->get();
+      $items = $this->searchOnElasticsearch($query);
+
+      return $this->buildCollection($items);
+    }
+
+    private function searchOnElasticsearch(string $query): array
+    {
+    	$instance = new Article;
+
+        $items = $this->search->search([
+            'index' => $instance->getSearchIndex(),
+            'type' => $instance->getSearchType(),
+            'body' => [
+                'query' => [
+                    'multi_match' => [
+                    	'fields' => ['title', 'body', 'tags'],
+                        'query' => $query,
+                    ],
+                ],
+            ],
+        ]);
+
+        return $items;
+    }
+
+
+    private function buildCollection(array $items): Collection
+    {
+        /**
+         * The data comes in a structure like this:
+         *
+         * [
+         *      'hits' => [
+         *          'hits' => [
+         *              [ '_source' => 1 ],
+         *              [ '_source' => 2 ],
+         *          ]
+         *      ]
+         * ]
+         *
+         * And we only care about the _source of the documents.
+        */
+        $hits = array_pluck($items['hits']['hits'], '_source') ?: [];
+
+        $sources = array_map(function ($source) {
+            // The hydrate method will try to decode this
+            // field but ES gives us an array already.
+            $source['tags'] = json_encode($source['tags']);
+            return $source;
+        }, $hits);
+
+        // We have to convert the results array into Eloquent Models.
+        return Article::hydrate($sources);
     }
 }
