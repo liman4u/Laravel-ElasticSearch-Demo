d96eaf0d7878726ef18bd8466b698d4b3c8a8cc8
Liman Adamu Labaran <adamu.labaran@callenssolutions.com>
2018-03-30 12:58:12 +0000

    Switch on search enabled in app service prodvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use App\Articles\ArticlesRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Articles\EloquentArticlesRepository;
+use App\Articles\ElasticsearchArticlesRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,8 +29,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(ArticlesRepository::class, function () {
-            return new EloquentArticlesRepository();
+      $this->app->singleton(ArticlesRepository::class, function($app) {
+          // This is useful in case we want to turn-off our
+          // search cluster or when deploying the search
+          // to a live, running application at first.
+          if (!config('services.search.enabled')) {
+              return new EloquentArticlesRepository();
+          }
+
+          return new ElasticsearchArticlesRepository(
+              $app->make(Client::class)
+          );
+      });
+
+      $this->bindSearchClient();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts(config('services.search.hosts'))
+                ->build();
         });
     }
 }
